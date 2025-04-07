4c8ede95ca07d4f32919a3091088bda8547fa6f4
Arqila <165964082+pablonification@users.noreply.github.com>
2025-04-07 20:49:37 +0700

    feat: add redirect to chatbot

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { redirect } from "next/navigation";
+
+export default function RedirectPage() {
+  return redirect("https://pablonification-rag-gemini2-0-pablo.hf.space/");
+}
